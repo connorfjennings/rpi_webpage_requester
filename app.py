cda641d6173ff145fa370d3fd5c4b324b0e3dd1c
Kenan Anderson <kenananderson@berkeley.edu>
2021-10-23 14:53:09 -0700

    fixed + cooler

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template(index.html)
+	return render_template("index.html")
 
 @app.route('/open')
 def open():
 	website = request.args['website']
-	return "works maybe"
+	radio = request.args['method']
+	return render_template("index.html")
 
 if __name__ == '__main__':
 	app.run(debug=False, host='0.0.0.0')
