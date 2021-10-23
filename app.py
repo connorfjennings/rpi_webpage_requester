17eebe6b781d53f97c2587e693adea4693c7d04a
Kenan Anderson <kenananderson@berkeley.edu>
2021-10-23 15:43:11 -0700

    prepare voting

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+videoQ = []
+
 
 @app.route('/')
 def index():
 	return render_template("index.html")
 
+
 @app.route('/open')
 def open():
 	website = request.args['website']
 	radio = request.args['method']
+	if (radio == "video"):
+		videoQ.append(website)
 	return render_template("index.html")
 
+@app.route('/remove')
+def remove():
+	website = request.args[website]
+	videoQ.remove(website)
+	return render_template("index.html")
+	
 if __name__ == '__main__':
 	app.run(debug=False, host='0.0.0.0')
