24690a236dda1ca1f5a49f2e0a71ff6cdde122bf
Kenan Anderson <kenananderson@berkeley.edu>
2021-10-23 17:22:04 -0700

    added separate queue thread with synchronization

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, render_template
-
+import threading
 app = Flask(__name__)
 videoQ = []
+Qsema = threading.Semaphore(value=0)
+def runQueue():
+	while True:
+		Qsema.acquire()
+		print("HERE IT IS: " + videoQ.pop(0))
 
 
 @app.route('/')
@@ -13,15 +18,17 @@ def index():
 def open():
 	website = request.args['website']
 	radio = request.args['method']
-	if (radio == "video"):
+	if (radio == "Video"):
 		videoQ.append(website)
+		Qsema.release()
 	return render_template("index.html")
 
-@app.route('/remove')
-def remove():
-	website = request.args[website]
-	videoQ.remove(website)
+@app.route('/vote')
+def vote():
+	website = request.args['website']
 	return render_template("index.html")
-	
+
 if __name__ == '__main__':
+	qThread = threading.Thread(target=runQueue, daemon=True)
+	qThread.start()
 	app.run(debug=False, host='0.0.0.0')
